e4e7d5e73d5b438331718e477fb608fab6e10408
KCPickleballer <34458947+KCPickleballer@users.noreply.github.com>
2018-11-05 20:49:21 -0600

    recovered deleted static folder

--- TicketMaster_App/static/js/app.js
+++ TicketMaster_App/static/js/app.js
@@ -1,34 +1,187 @@
-// function buildMetadata(sample) {
 
-//   // @TODO: Complete the following function that builds the metadata panel
+ function loadMarkersTest(dataTest) {
+  //console.log('in loadMarkers map: ', myMap);
+  //console.log('in loadMarkers data: ', dataTest);
 
-//   // Use `d3.json` to fetch the metadata for a sample
+ }
+
+function buildScatter(city){
+
+  d3.json(`/chart/${city}`).then(function(data) {
+
+    console.log('bldScatter: ', data);
+
+
+   var size = [];
+   for (var m = 0; m < data.EventCnt.length; m++){
+    size.push(data.EventCnt[m] + 5);
+   }
+
+      
+  console.log('size', size[0]);
+  var color = []
+  for (var l = 0; l < data.Classification.length; l++){
+      switch (data.Classification[l]){
+        case ("Sports"): 
+          color.push("red");
+          break;
+        case ("Arts & Theatre"):
+          color.push("yellow");
+          break;
+        case ("Music"):
+          color.push('blue');
+          break;
+        case ("Film"):
+          color.push("Yellow");
+          break;
+        default:
+          color.push("orange");
+      }
+  }
+
+    var week = data.Week
+    var event_count = data.EventCnt  
+    var class_name = data.Classification
+
+
+    var trace1 = {
+      type: "scatter",
+      mode: "markers",
+      name: class_name,
+      x: week,
+      y: event_count,
+      text : class_name,
+      marker: {
+        color: color,
+        size: size
+      }
+    };
+
+     var data1 = [trace1];
+
+     var layout = {
+      title: "Event Count Per Week",
+      xaxis: { title: "Week" }
+    };
+   
+    Plotly.newPlot("bubble", data1, layout);
+
+
+  });  
+
+
+
+}
+
+function buildVenueList(city){
+  d3.json(`/venue/${city}`).then(function(data) {
+    venueData = data;
+    console.log('venue data: ', venueData);
+
+    var selector = d3.select("#selVenue");
+
+    selector.selectAll("*").remove();
+
+    selector
+    .append("option")
+    .text("Select a Venue")
+    .property("value", "Select a Venue");
+
+        
+     for (var j = 0; j < data.venue.length; j++) {
+      console.log('in v loop: ', data.venue[j]);
+      //     cityNames.forEach((city) => {
+     selector
+       .append("option")
+       .text(data.venue[j])
+       .property("value", data.venue[j]);
+    }
+    //firstCity =  cityNames.city[0];
+    //console.log('t99:', firstCity);
+
+  });
+
+}
+
+
+
+
+function loadMarkers(mapData){
+  console.log('mapData', mapData);
+  console.log('what: ', myMap);
   
-//   d3.json(`/metadata/${sample}`).then(function(data) {
+  var lng = -94.48
+  var lat = 39.10
 
-//     console.log('metatest: ', data);
+ switch (mapData.City_name[0]) {
+  case ("Chicago"): 
+    lat = 41.88; 
+    lng = -87.63;
+    break;
+  case ( "Kansas City" ): 
+    console.log('in switch kc');
+    lat = 39.10; 
+    lng = -94.48;
+    break;
+  case ("Las Vegas" ):     
+    lat = 36.11; 
+    lng = -115.17;
+    break;
+  case ("Miami" ):
+    lat = 25.77; 
+    lng = -80.19;
+    break;
+  case ("Milwaukee" ):
+    lat = 43.03; 
+    lng = -87.90;
+    break;
+  case ("Nashville" ):       
+    lat = 36.17; 
+    lng = -86.77;
+    break;
+  case ("New Orleans" ):         
+    lat = 29.95; 
+    lng = -90.07;
+    break;
+  case ( "Seattle" ):     
+    lat = 47.60; 
+    lng = -122.33;
+    break;
+  case ("Boston" ):
+    lat = 42.35; 
+    lng = -87.63;
+    break;
+  case ("Denver" ):
+    lat = 39.73; 
+    lng = -104.98;    
+    break;
+}
+ // console.log('lat', lat);
+
+    
+     console.log('lat', myMap);
+     latlng = L.latLng(lat, lng);
   
-//     // Use d3 to select the panel with id of `#sample-metadata`
-//   d3.select("#sample-metadata").html("")
-//   d3.select("#sample-metadata").append("tbody").html("")
-
-//     // Use `.html("") to clear any existing metadata
-
-//     for (const [key, value] of Object.entries(data)) {
-//      // console.log(`${key} ${value}`); 
-//      d3.select("tbody")
-//      .append("tr")
-//      .html(`<td>${key}:</td><td>${value}</td>`)
-//     }
-//     // Use `Object.entries` to add each key and value pair to the panel
-//     // Hint: Inside the loop, you will need to use d3 to append new
-//     // tags for each key-value in the metadata.
-//   });
-//     // BONUS: Build the Gauge Chart
-//     // buildGauge(data.WFREQ);
-// }
+     myMap.panTo(new L.LatLng(lat, lng));
+  
+  markers = L.markerClusterGroup();
+  
+   // Loop through data
+  for (var i = 0; i < mapData.Latitude.length; i++) {
+  
+    markers.addLayer(L.marker([mapData.Longitude[i], mapData.Latitude[i]])
+    //.bindPopup(mapData.eventname[i]));      
+    .bindPopup("<h3>" + mapData.eventname[i] + "<h3><h3>Venue: " + mapData.Venue[i] + "<h3> <h3>Date:" + mapData.Date[i] + "<h3>"));
 
 
+
+  }
+  
+    // Add our marker cluster layer to the map
+    console.log('b4 markers')
+    myMap.addLayer(markers);
+  
+}
 
 function buildCharts(city) {
   var today = new Date();
@@ -55,35 +208,13 @@ function buildCharts(city) {
     d3.json(`/cities/${city}`).then(function(data) {
 
      console.log('test999: ', data);
+     cityDataTest = data;
+     loadMarkers(cityDataTest);
 
      var graphDate = [];
      var graphWk = ''
 
-     for (var k = 0; k < data.Date.length; k++) {
-      var dteStr = data.Date[k].split("/");
-      
-      //adjust month to 2 digits
-      if (parseInt(dteStr[0]) >= 1 && parseInt(dteStr[0]) <= 9) {dteStr[0]  = '0' + dteStr[0] }
-       // week 1 week 2 logic
-       if (parseInt(dteStr[1]) >= 1 && parseInt(dteStr[1]) <= 7) {graphDate[k] = dteStr[0] + '-WK1'}
-       else if (parseInt(dteStr[1]) >= 8 && parseInt(dteStr[1]) <= 14) {graphDate[k] = dteStr[0] + '-WK2'}
-       else if (parseInt(dteStr[1]) >= 15 && parseInt(dteStr[1]) <= 21) {graphDate[k] = dteStr[0] + '-WK3'}
-       else {graphDate[k] = dteStr[0] + '-WK4'}
-
-      // adjust montyh 2 2 digits 
-      if (parseInt(dteStr[1]) >= 1 && parseInt(dteStr[1]) <= 9) {dteStr[1]  = '0' + dteStr[1] }
-             
-      
-       data.Date[k] = dteStr[2] + '-' + dteStr[0] + '-' + dteStr[1]
-
-          
-     }
-
-     // add graphDate element to data object
-     data['graphDate'] = graphDate;
-
-     console.log('test99: ', data);
-
+   
 
 
     const classLabels = ["Sports", "Music", "Arts & Theatre",  "Miscellaneous", "Film"];
@@ -111,158 +242,39 @@ function buildCharts(city) {
     }
     
     console.log(city, classValues)
-    
-      
-     
  
 
-//    const idsSlice = data.otu_ids.slice(0, 10);
-//    const sampValSlice = data.sample_values.slice(0, 10);
-//    const lblsSlice =  data.otu_labels.slice(0, 10);
-// console.log('scliced: ', sampValSlice);
-
-   
+  
 
   var trace1 = {
    labels: classLabels,
    values: classValues,
    type: 'pie',
    text: classLabels
- };
+  };
 
- var data2 = [trace1];
-
-// var layout = {
-//   title: "'Bar' Chart",
-// };
-
- Plotly.newPlot("pie", data2);
+  var data2 = [trace1];
 
 
+  Plotly.newPlot("pie", data2);
 
+  });
 
-
-    });
-  }   
-
-//   // @TODO: Use `d3.json` to fetch the sample data for the plots
-//   // d3.json(`/samples/${sample}`).then((data) => {
-//   //   console.log(data);
-//   //   });
-//   // const dataPromise = d3.json(`/samples/${sample}`).then(function(data){
-//   //   buildPie(data);
-
-//   // });  
-  
-  
-
-
-//   d3.json(`/samples/${sample}`).then(function(data) {
-
-//     console.log('test1: ', data);
+  buildScatter(city)
     
+}   
 
-
-
-//     var ids = data.otu_ids
-//     var sample_val = data.sample_values  
-//     var txtLables = data.otu_lables
-
-//     //var sortObj = data
-
-//     console.log('otu: ', ids[0]);
-//     console.log('samps: ', sample_val[0]);
-    
-//     var trace1 = {
-//       type: "scatter",
-//       mode: "markers",
-//       name: ids,
-//       x: ids,
-//       y: sample_val,
-//       text : txtLables,
-//       marker: {
-//         color: ids,
-//         size: sample_val
-//       }
-//     };
-
-//      var data1 = [trace1];
-
-//      var layout = {
-//       title: "Sample Data",
-//       xaxis: { title: "OTU ID" }
-//     };
-     
-
-//     // var layout = {
-//     //   title: `${stock} closing prices`,
-//     //   xaxis: {
-//     //     range: [startDate, endDate],
-//     //     type: "date"
-//     //   },
-//     //   yaxis: {
-//     //     autorange: true,
-//     //     type: "linear"
-//     //   }
-
-//     Plotly.newPlot("bubble", data1, layout);
-
-//    // buildPie(data);
-
-//    // sortObj.sort((a, b) => Number(b.sample_values) - Number(a.sample_values));
-
-//    const idsSlice = data.otu_ids.slice(0, 10);
-//    const sampValSlice = data.sample_values.slice(0, 10);
-//    const lblsSlice =  data.otu_labels.slice(0, 10);
-// console.log('scliced: ', sampValSlice);
-
-   
-
-//   var trace1 = {
-//    labels: idsSlice,
-//    values: sampValSlice,
-//    type: 'pie'
-//   // text: lblsSlice
-//  };
-
-//  var data2 = [trace1];
-
-// // var layout = {
-// //   title: "'Bar' Chart",
-// // };
-
-//  Plotly.newPlot("pie", data2);
-
-
-//     });
-
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-   
-   
-
-    // const dataPromise = d3.json(`/samples/${sample}`);
-    // console.log("Data Promise: ", dataPromise);
-    // console.log("data: ", dataPromise.promiseValue.otu_ids)
-   
-//}
+var  cityDataTest = {};
 
 function init() {
   var firstCity = 'Kansas City'
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-   //selector.on("change", optionChanged );
-  // Use the list of sample names to populate the select options
+
 
   d3.json("/names").then(function(cityNames) {
       
      for (var j = 0; j < cityNames.city.length; j++) {
-       //console.log('test99: ', cityNames.baloney[j]);
-       //     cityNames.forEach((city) => {
       selector
         .append("option")
         .text(cityNames.city[j])
@@ -274,35 +286,72 @@ function init() {
     });
     //console.log('t100:', firstCity);
    
-
-     buildCharts(firstCity);
-
+    buildVenueList(firstCity);
+  
+    buildCharts(firstCity);
+    
+    console.log('b4 init load Markers ',cityDataTest);
 
    }
 
-//   d3.json("/names").then((cityNames) => {
-//     cityNames.forEach((city) => {
-//       selector
-//         .append("option")
-//         .text(city)
-//         .property("value", city);
-//     });
-    
-//     // Use the first sample from the list to build the initial plots
-//      const firstCity = cityNames[0];
-//     //     console.log('t1:', firstCity);
-
-//      buildCharts(firstCity);
-// //   //  buildMetadata(firstCity);
-//    });
-// }
 
 function optionChanged(newCity) {
   // Fetch new data each time a new sample is selected
+  //clear venue table
+  var tbody = d3.select("tbody");
+  tbody.selectAll("*").remove();
+
   console.log(newCity);
   buildCharts(newCity);
-//  buildMetadata(newCity);
+  buildVenueList(newCity);
+
 }
 
 // Initialize the dashboard
+
+var venueData = {};
+
+var FirstTime = true;
+var    myMap = L.map("map", {
+  //default to Kansas City,
+  center: [39.10, -94.48],
+  zoom: 11
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ"
+
+}).addTo(myMap);
+
+FirstTime = false;
+
+function venueChanged(newVenue){
+  console.log('Venue changed: ', newVenue);
+
+  var tbody = d3.select("tbody");
+  tbody.selectAll("*").remove();
+
+  d3.json(`/venueinfo/${newVenue}`).then(function(data) {
+    console.log('url', data.url[0])
+    
+    // Append one table row `tr` to the table body
+    var row = tbody.append("tr");
+    
+    // Append one cell for the student name
+    row.append("td").text(data.venue[0]);
+    
+    // Append one cell for the student grade
+    row.append("td").text(data.address[0]);
+    row.append("td").append("a").attr("href", data.url[0]).text(data.url[0]);
+    
+  }); 
+
+}
+
+
 init();
+
